1d38eedbfe6cf31a55812f58c5725235fc04c603
Jflinchum <jonflinchum@gmail.com>
2018-01-26 15:19:21 -0500

    Gray scaled, blurred, and applied Otsu's method to image.

--- main.py
+++ main.py
@@ -10,7 +10,12 @@ def main():
 
     # Take the argument and open the image using opencv
     img = cv2.imread(sys.argv[1])
-    img = img[:,:,0]
+    # Make the image grey
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # Blur the image a little bit
+    img = cv2.GaussianBlur(img,(5,5),0)
+    # Applied Otsu's Method
+    ret, img = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
     cv2.imshow("image", img)
     cv2.waitKey(0)
 
