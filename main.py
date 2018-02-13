b02c0e306684146843e6738008022c1fcec46885
Jflinchum <jonflinchum@gmail.com>
2018-02-12 23:02:55 -0500

    Cleaned up main and added comments.

--- main.py
+++ main.py
@@ -29,41 +29,59 @@ def main():
         # Draw the contours onto the original video frame
         drawContours(croppedHand, maskedHand)
 
+        # Text for if the background subtraction is training
         if currentFrame < trainingFrames:
             cv2.putText(image, "Training...", (10, 600), cv2.FONT_HERSHEY_SCRIPT_COMPLEX, 2, (255, 255, 255), 2)
 
+        # Text for capture mode
         if captureMode:
             cv2.putText(image, "Capture Mode", (10, 50), cv2.FONT_HERSHEY_SCRIPT_COMPLEX, 2, (70, 0, 255), 2)
 
+        # Box for where the hand is cropped
         cv2.rectangle(image, (500, 500), (100, 100), (0, 255, 0), 0)
 
         # Show the frame
         cv2.imshow("video", image)
-        cv2.imshow("mask", maskedHand)
 
+        # Wait for a key press
         key = cv2.waitKey(10)
 
         if captureMode: 
+            # Cancel capture mode on a space keypress
             if key == ord(" "):
                 captureMode = False
+            # If the key is lowercase alphabet letter
             elif key >= 97 and key <= 122:
+                # Generate random numbers so there are no file collisions
                 rand = randint(0, 100000)
+                randFlip = randint(0, 100000)
+
+                # We need a mirror image for left and right handed folks
+                flipMask = cv2.flip(maskedHand)
                 filename = chr(key) + str(rand) + ".jpg"
+                filenameFlip = chr(key) + str(randFlip) + ".jpg"
+
+                # Write both to filename
                 cv2.imwrite(filename, maskedHand)
+                cv2.imwrite(filenameFlip, flipMask)
+
                 print ("Saved as " + filename) 
+                print ("Saved as " + filenameFlip) 
         else:
             # If space bar is entered, return to end program
             if key == ord(" "):
                 return
+
             # If r is pressed, reset the frame counter and
             # re-train the background detection
             elif key == ord("r"):
                 currentFrame = 0
+            # Turn on capture mode on c key press
             elif key == ord("c"):
                 captureMode = True
+            # Attempt to match the hand 
             elif key == ord("t"):
                 templateMatch(maskedHand)
-            
 
 
 if __name__ == "__main__":
