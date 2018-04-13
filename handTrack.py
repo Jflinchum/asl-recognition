3b6526575cce5f9074ffbc530614e55fa7c9e016
Jflinchum <jonflinchum@gmail.com>
2018-04-13 19:17:05 -0400

    Averaged out the center of the palm over a period of 5 frames.

--- handTrack.py
+++ handTrack.py
@@ -4,6 +4,9 @@ import numpy as np
 fgbg = cv2.createBackgroundSubtractorMOG2()
 trained = False
 
+previousPalms = []
+maxPalms = 5
+
 def isTrained():
     global trained
     return trained
@@ -37,6 +40,7 @@ def getMask(img, currentFrame, trainingFrames = 100):
 # Finds the contours of the imgCopy, presumably the mask of img, and 
 # draws the contours onto the img
 def drawContours(img, imgCopy):
+    global previousPalms
     # Find the contours in the image
     image, contours, hierarchy = cv2.findContours(imgCopy, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
     
@@ -79,15 +83,22 @@ def drawContours(img, imgCopy):
                 cv2.line(img, start, end, [0,255,0], 2)
                 cv2.circle(img, far, 8, [150, 0, 150], -1)
 
-            # Averaging the center of the palm
+            # Estimating palm center
             if (farPoints != 0):
                 palmCenterX = int(palmCenter[0]/(farPoints))
                 palmCenterY = int(palmCenter[1]/(farPoints))
                 palmCenter = palmCenterX, int((palmCenterY+highestValue)/2)
-                cv2.circle(img, palmCenter, 8, [255,0,0], -1)
                 if highestValue < y+h:
                     h = highestValue - y
+               
+                # Averaging palm center with past palm centers
+                if len(previousPalms) >= maxPalms:
+                    previousPalms.pop(0)
+                previousPalms.append(palmCenter)
+                palmCenter = tuple(np.mean(previousPalms, axis=0, dtype=int))
+
         # Drawing the box and contours
+        cv2.circle(img, palmCenter, 8, [255,0,0], -1)
         cv2.rectangle(img, (x, y), (x+w, y+h), (0, 0, 255), 3)
         cv2.drawContours(img, [maxContour], 0, (255, 0, 0), 3)
     return maxContour
